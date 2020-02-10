949ebcf8c85e5d8d43a0d638215e30c7a91d5d03
Arttu <pesonen.arttu97@gmail.com>
2020-02-10 15:04:16 +0200

    Added jpa and H2

--- src/main/java/com/example/Bookstore/BookstoreApplication.java
+++ src/main/java/com/example/Bookstore/BookstoreApplication.java
@@ -1,13 +1,29 @@
 package com.example.Bookstore;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.example.Bookstore.domain.Book;
+import com.example.Bookstore.domain.BookRepository;
 
 @SpringBootApplication
 public class BookstoreApplication {
+	private static final Logger log = LoggerFactory.getLogger(BookstoreApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
+	}
+	
+	@Bean
+	public CommandLineRunner book(BookRepository repository) {
+		return (args) -> {
+			repository.save(new Book("Catcher in the rye", "Some dude who likes the n word", 1969, "A1111", 19.99));
+			repository.save(new Book("Tuntematon Sotilas", "Väinö Linna", 1948, "B1111", 40.00));
+		};
 	}
 
 }
--- src/main/java/com/example/Bookstore/domain/Book.java
+++ src/main/java/com/example/Bookstore/domain/Book.java
@@ -1,6 +1,17 @@
 package com.example.Bookstore.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+
 public class Book {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+	
 	private String title;
 	private String author;
 	private int year;
@@ -54,5 +65,9 @@ public class Book {
 	
 	public void setPrice(double price) {
 		this.price = price;
+	}
+	
+	public String toString() {
+		return this.getTitle() + " " + this.getAuthor() + " " + this.getYear() + " " + this.getIsbn() + " " + this.getPrice();
 	}
 }
