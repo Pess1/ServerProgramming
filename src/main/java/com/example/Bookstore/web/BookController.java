843c4e27b28ef9112537ccce3d8a7efc33dfa936
Arttu <pesonen.arttu97@gmail.com>
2020-02-24 20:19:56 +0200

    Added REST services

--- src/main/java/com/example/Bookstore/web/BookController.java
+++ src/main/java/com/example/Bookstore/web/BookController.java
@@ -1,11 +1,16 @@
 package com.example.Bookstore.web;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.atomic.AtomicLong;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.example.Bookstore.domain.Book;
 import com.example.Bookstore.domain.BookRepository;
@@ -13,6 +18,7 @@ import com.example.Bookstore.domain.CategoryRepository;
 
 @Controller
 public class BookController {
+	private final AtomicLong counter = new AtomicLong();
 	
 	@Autowired
 	private BookRepository brepository;
@@ -65,6 +71,16 @@ public class BookController {
 		brepository.save(book);
 		return "redirect:booklist";
 		
+	}
+	
+	@RequestMapping(value="books", method = RequestMethod.GET)
+	public @ResponseBody List<Book> bookListRest() {
+		return (List<Book>) brepository.findAll();
+	}
+	
+	@RequestMapping(value="books/{id}", method = RequestMethod.GET)
+	public @ResponseBody Optional<Book> findStudentRest(@PathVariable("id") Long id) {
+		return brepository.findById(id);
 	}
 
 }
